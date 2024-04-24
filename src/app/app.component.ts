a812d2ee732720bc6be11f28fdcae953d9db35c4
Rojas Córsico Ivana Soledad <irojas@gmail.com>
2024-04-24 13:56:29 -0300

    ngModel directive example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular17-directives-ngModel';
+  name="";
+
 }
